c1aff6f198588742b61d91a614e43411b3f69361
AllanVasconceL0s <allanvasconcelosfn@gmail.com>
2026-03-19 18:57:21 -0300

    Melhorando a estrutura do código com tipagem e docstring

--- src/load.py
+++ src/load.py
@@ -5,8 +5,18 @@ class Load:
     def __init__(self):
         pass
 
-    def create_sqlite_table(self, universities_list, db_name, table_name):
+    def create_sqlite_table(
+        self, universities_list: list[dict], db_name: str, table_name: str
+    ):
+        """
+        Método responsável por criar o banco SQL e adicionar tabelas e dados.
 
+        Args:
+            universities_list (list): Lista de dicionários contendo informações das universidades.
+            db_name (str): Nome do banco de dados a ser criado.
+            table_name (str): Nome da tabela a ser criada no banco de dados.
+        """
+        # Criar o banco e se concectar nele
         # Criar o banco e se concectar nele
         con = sqlite3.connect(f"{db_name}.db")
         c = con.cursor()
